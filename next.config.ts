2f7781793b3cfc82d1ceb9932ce21265308dcf34
Mateusz Kulesza <ev45ive@gmail.com>
2025-07-27 14:20:38 +0200

    IDBot

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+import { withBotId } from 'botid/next/config';
+ 
+const nextConfig = {
+  // Your existing Next.js config
 };
-
-export default nextConfig;
+ 
+export default withBotId(nextConfig);
