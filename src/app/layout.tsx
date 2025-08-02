8249ad4cf653ccaf16f24dcc0089655096b9a738
Mateusz Kulesza <ev45ive@gmail.com>
2025-08-02 19:59:59 +0200

    Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,14 +26,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="googletagmanager" strategy="beforeInteractive">
+        {`<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env["NEXT_PUBLIC_GTM"]}');</script>
+<!-- End Google Tag Manager -->`}
+      </Script>
+      
       {/* Cloudflare CAPTCHA */}
       <Script
         src="https://challenges.cloudflare.com/turnstile/v0/api.js"
         strategy="afterInteractive"
       />
 
+
       {/* Google Analytics */}
-      <Script
+      {/* <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
@@ -45,7 +56,7 @@ export default function RootLayout({
             gtag('js', new Date());
             gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
           `}
-      </Script>
+      </Script> */}
       <body className={`bg-white`}>{children}</body>
     </html>
   );
